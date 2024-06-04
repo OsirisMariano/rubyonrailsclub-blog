3f4445197e058c2fb2d965e1cda29d204f840a65
Osiris Mariano <oslimacontato@gmail.com>
2024-06-04 20:12:21 -0300

    Migrando o template do artigo

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -18,91 +16,83 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_13_224636) do
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string("name", null: false)
-    t.string("record_type", null: false)
-    t.uuid("record_id", null: false)
-    t.uuid("blob_id", null: false)
-    t.datetime("created_at", null: false)
-    t.index(["blob_id"], name: "index_active_storage_attachments_on_blob_id")
-    t.index(
-      ["record_type", "record_id", "name", "blob_id"],
-      name: "index_active_storage_attachments_uniqueness",
-      unique: true,
-    )
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.uuid "record_id", null: false
+    t.uuid "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string("key", null: false)
-    t.string("filename", null: false)
-    t.string("content_type")
-    t.text("metadata")
-    t.string("service_name", null: false)
-    t.bigint("byte_size", null: false)
-    t.string("checksum")
-    t.datetime("created_at", null: false)
-    t.index(["key"], name: "index_active_storage_blobs_on_key", unique: true)
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "active_storage_variant_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid("blob_id", null: false)
-    t.string("variation_digest", null: false)
-    t.index(["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true)
+    t.uuid "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "admins", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string("email", default: "", null: false)
-    t.string("encrypted_password", default: "", null: false)
-    t.string("reset_password_token")
-    t.datetime("reset_password_sent_at")
-    t.datetime("remember_created_at")
-    t.datetime("created_at", null: false)
-    t.datetime("updated_at", null: false)
-    t.index(["email"], name: "index_admins_on_email", unique: true)
-    t.index(["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true)
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "articles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string("title")
-    t.text("body")
-    t.datetime("created_at", null: false)
-    t.datetime("updated_at", null: false)
-    t.string("slug")
-    t.uuid("category_id")
-    t.index(["category_id"], name: "index_articles_on_category_id")
-    t.index(["slug"], name: "index_articles_on_slug", unique: true)
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "slug"
+    t.uuid "category_id"
+    t.index ["category_id"], name: "index_articles_on_category_id"
+    t.index ["slug"], name: "index_articles_on_slug", unique: true
   end
 
   create_table "categories", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string("name")
-    t.datetime("created_at", null: false)
-    t.datetime("updated_at", null: false)
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string("slug", null: false)
-    t.integer("sluggable_id", null: false)
-    t.string("sluggable_type", limit: 50)
-    t.string("scope")
-    t.datetime("created_at")
-    t.index(
-      ["slug", "sluggable_type", "scope"],
-      name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope",
-      unique: true,
-    )
-    t.index(["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type")
-    t.index(["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id")
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string("email", default: "", null: false)
-    t.string("encrypted_password", default: "", null: false)
-    t.string("reset_password_token")
-    t.datetime("reset_password_sent_at")
-    t.datetime("remember_created_at")
-    t.datetime("created_at", null: false)
-    t.datetime("updated_at", null: false)
-    t.index(["email"], name: "index_users_on_email", unique: true)
-    t.index(["reset_password_token"], name: "index_users_on_reset_password_token", unique: true)
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
